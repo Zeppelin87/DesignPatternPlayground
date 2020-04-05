c31f9cc8f127f63f6a8fe7709a1052dce7d6ae5f
anken346@yahoo.com <anken3446@yahoo.com>
2020-04-05 00:15:34 -0500

    first commit

---
+++ SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    // Any class should have one reason to change.
+
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; // memento
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        public void Save(string filename)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+
+        private static Journal Load(string filename)
+        {
+            return new Journal();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var journal = new Journal();
+            journal.AddEntry("I ate food.");
+            journal.AddEntry("I went for a run.");
+            Console.WriteLine(journal);
+        }
+    }
+}
