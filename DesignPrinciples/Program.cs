f0f856ef4cba293f066ff60b4ee293e9770d3be4
anken346@yahoo.com <anken3446@yahoo.com>
2020-04-05 00:44:15 -0500

    adding new proj that will parent all design principles

---
+++ DesignPrinciples/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DesignPrinciples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleResponsibilityPrinciple();
+        }
+
+        private static void SingleResponsibilityPrinciple()
+        {
+            var journal = new Journal();
+            journal.AddEntry("I ate food.");
+            journal.AddEntry("I went for a run.");
+            Console.WriteLine(journal);
+
+            var persistence = new Persistence();
+            var filename = @"c:\temp\journal.txt";
+            persistence.SaveToFile(journal, filename, true);
+            Process.Start(filename);
+        }
+    }
+}
---
+++ DesignPrinciples/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DesignPrinciples
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; // memento
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+            {
+                File.WriteAllText(filename, journal.ToString());
+            }
+        }
+
+        public static Journal Load(string filename)
+        {
+            return new Journal();
+        }
+    }
+}
