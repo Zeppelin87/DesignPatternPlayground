08dab1a8d94f147ef645d6ba7333ccf4d7a03938
anken346@yahoo.com <anken3446@yahoo.com>
2020-04-10 17:10:05 -0500

    Design Patterns - Structural - Adapter - Vector/Raster Demo

--- DesignPatterPlayground/Program.cs
+++ DesignPatterPlayground/Program.cs
@@ -10,6 +10,7 @@ using DesignPatterPlayground.DesignPatterns.Creational.Prototype.ExplicitDeepCop
 using DesignPatterPlayground.DesignPatterns.Creational.Singleton.SingletonDependencyInjectionExample;
 using DesignPatterPlayground.DesignPatterns.Creational.Singleton.SingletonImplementationExample;
 using DesignPatterPlayground.DesignPatterns.Creational.Singleton.TestabilityIssuesExample;
+using DesignPatterPlayground.DesignPatterns.Structural.Adapter.VectorRasterExample;
 using DesignPatterPlayground.SolidPrinciples;
 
 namespace DesignPatterPlayground
@@ -19,34 +20,49 @@ namespace DesignPatterPlayground
         static void Main(string[] args)
         {
             //SOLIDPrinciples();
-            CreationalPatterns();
+            //Creational();
+            Structural();
         }
 
-        private static void CreationalPatterns()
+        #region Creational
+
+        private static void Creational()
         {
-            // 1. Builder Design Patterns
-            //Builder.Run();
-            //FluentBuilderInheritance.Run();
-            //FunctionalBuilder.Run();
-            //FacetedBuilder.Run();
+            // 1.Builder 
+            Builder.Run();
+            FluentBuilderInheritance.Run();
+            FunctionalBuilder.Run();
+            FacetedBuilder.Run();
 
-            // 2. Factory Design Patterns
-            //FactoryMethod.Run();
-            //Factory.Run();
-            //InnerFactory.Run();
-            //AbstractFactory.Run();
+            // 2.Factory 
+            FactoryMethod.Run();
+            Factory.Run();
+            InnerFactory.Run();
+            AbstractFactory.Run();
 
-            // 3. Prototype Design Patterns
-            //ICloneableIsBad.Run();
-            //CopyConstructors.Run();
-            //ExplicitDeepCopyInterface.Run();
-            //CopyThroughSerialization.Run();
+            // 3.Prototype 
+            ICloneableIsBad.Run();
+            CopyConstructors.Run();
+            ExplicitDeepCopyInterface.Run();
+            CopyThroughSerialization.Run();
 
-            // 4. Singleton Design Patterns
-            //SingletonImplementation.Run();
-            //TestabilityIssues.Run();
+            // 4.Singleton 
+            SingletonImplementation.Run();
+            TestabilityIssues.Run();
             SingletonDependencyInjection.Run();
         }
+
+        #endregion
+
+        #region Structural
+
+        private static void Structural()
+        {
+            // 1. Adapter
+            VectorRasterDemo.Run();
+        }
+
+        #endregion
 
         #region S.O.I.L.D. Principles
 
